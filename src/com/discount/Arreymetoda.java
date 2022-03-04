9383b36b5751f5434e3e4821d4876b1944aad0de
Micilucic <lucicmili@gmail.com>
2022-03-04 12:27:35 +0100

    Neue Methode

---
+++ src/com/discount/Arreymetoda.java
@@ -0,0 +1,34 @@
+package com.discount;
+
+public class Arreymetoda {
+    public static void main(String[] args) {
+        int[] schachtel = new int[]{3, 5, 7, 9, 11, 13, 17}; //Primzahlen = dividierbar durch 1 und sich, sonst nichts
+        // Schachtel ist der Name von arrey
+
+        printArray(schachtel);
+        System.out.println(containsNumber(5, schachtel));
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (i == array.length - 1) {
+                System.out.print(array[i]);
+            } else {
+                System.out.print(array[i] + ", ");
+            }
+        }
+    }
+
+    public static boolean containsNumber(int number, int[] array) {
+        boolean isContained = false;
+        for (int i = 0; i < array.length; i++) {
+            if (number == array[i]) {
+                isContained = true;
+            } else {
+                isContained = false;
+            }
+        }
+        return isContained;
+    }
+}
+// System.out.print(array[i] + ", "); //statt println nur print schreiben, damit es nebeneinander steht und nicht untereinander !
