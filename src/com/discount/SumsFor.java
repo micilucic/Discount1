d2fceb6b1b3082c9e30a6411ac2e3647b1a95044
Micilucic <lucicmili@gmail.com>
2022-03-04 10:46:31 +0100

    ada

---
+++ src/com/discount/SumsFor.java
@@ -0,0 +1,14 @@
+package com.discount;
+
+public class SumsFor {
+    public static void main(String[] args) {
+        int summe = 0;
+
+        for (int i = 0; i < 10; i++) {
+            summe = summe + i;
+            // summe +=i; (Abkürzung)
+        }
+
+
+    }
+}
